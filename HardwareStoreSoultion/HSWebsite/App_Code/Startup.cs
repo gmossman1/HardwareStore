7d50591d9d894e092bd902b30a5eec32250f0fa0
gmossman1 <gmossman1@studentmail.nait.ca>
2015-11-19 15:59:24 -0700

    Creation of Hardware Store Website
    Educational Purpose Only

---
+++ HardwareStoreSoultion/HSWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HSWebsite.Startup))]
+namespace HSWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
